f8953e861142c51850ba36241b5ba32e3fe36fc1
BenceHordos523 <bence.hordos523@gmail.com>
2025-07-14 15:23:23 +0200

    textnode repr changed

--- src/textnode.py
+++ src/textnode.py
@@ -18,4 +18,4 @@ class TextNode:
         return self.text == text_node.text and self.text_type == text_node.text_type and self.url == text_node.url
     
     def __repr__(self):
-        return f"TEXTNODE({self.text}, {self.text_type}, {self.url})"
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
